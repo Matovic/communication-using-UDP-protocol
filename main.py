01107b4cfe0313dd40613508d86914fafea62531
Erik <matovic.er@gmail.com>
2020-11-25 02:20:30 +0100

    small changes

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import server
 
 def main():
     if len(sys.argv) != 2:
-        print("ERROR 00: Wrong paramaters!")
+        print("ERROR 00: Wrong parameters!")
         sys.exit(-1)
 
     if sys.argv[1] == 'client':
@@ -17,10 +17,9 @@ def main():
     elif sys.argv[1] == 'server':
         server.user_interface()
     else:
-        print("ERROR 00: Wrong paramaters!")
+        print("ERROR 00: Wrong parameters!")
         sys.exit(-1)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
